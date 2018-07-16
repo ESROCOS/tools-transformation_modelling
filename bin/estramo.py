de310cd6b054dfe299cd6d554ba191113751001b
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2018-07-16 17:17:43 +0200

    added test file, added xml parsing capabilities

--- bin/estramo.py
+++ bin/estramo.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python
 
 import xml.etree.cElementTree as ET
+from graphviz import Digraph
+from sets import Set
 
-TRANSFORMATIONS_FILE = "transformations.xml"
+HEADER = "TransformationHeader"
+FILE = "transformations.xml"
 
-tree = ET.parse(TRANSFORMATIONS_FILE)
-transformations = tree.getroot()
+tree = ET.parse(FILE)
+root = tree.getroot()
+
+requested = root.find("requested")
+provided = root.find("provided")
+
+frames = Set()
+transforms = Set()
+
+requested_transforms = Set()
+
+for child in requested:
+  header       = child.find(HEADER)
+  requested_transforms.add((header.get("target"),header.get("expressed_in")))
+
+for child in provided:
+  header       = child.find(HEADER)
+ 
+#  print header.tag,header.attrib
+#  print header.get("target"),header.get("expressed_in")
+
+  frames.add(header.get("target"))
+  frames.add(header.get("expressed_in"))
+    
+  transforms.add((header.get("target"),header.get("expressed_in")))
+
+print "frames:\t\t",frames
+
+print "transforms:\t",transforms
+
+print "requests:\t",requested_transforms
+
